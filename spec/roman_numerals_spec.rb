936bd687307b04c54187fc67fd4478fa8af9e457
williamjones <willj836@gmail.com>
2022-11-01 17:16:15 +0000

    passes all tests, valid for number up to 3999

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -16,4 +16,20 @@ describe 'to_roman_numeral' do
   it "should be able to convert 35 to XXXV" do 
     expect(35.to_roman_numeral).to eq('XXXV')
   end
+
+  it "should be able to convert 996 to CMXCVI" do 
+    expect(996.to_roman_numeral).to eq('CMXCVI')
+  end
+
+  it "should be able to convert 2474 to MMCDLXXIV" do 
+    expect(2474.to_roman_numeral).to eq('MMCDLXXIV')
+  end
+
+  it "should be able to convert 3000 to MMM" do 
+    expect(3000.to_roman_numeral).to eq('MMM')
+  end
+
+  it "should be able to convert 3999 to MMMCMXCIX" do 
+    expect(3999.to_roman_numeral).to eq('MMMCMXCIX')
+  end
 end
